f90163495f6aeeb1132150e8849846b824dc007c
SimplifyMSP <31136477+SimplifyMSP@users.noreply.github.com>
2018-08-01 17:25:01 -0400

    Add files via upload

---
+++ TextReplace/TextReplace/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TextReplace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 55);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose Folder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(435, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(516, 81);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Parameters";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text to Replace:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Replace With:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(402, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(108, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(402, 23);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(447, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(536, 185);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Bahnschrift SemiLight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TextReplace";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TextReplace/TextReplace/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace TextReplace
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            //Create new folder dialog
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //Show the dialog to search for a folder and if the user has read/write access to that folder then,
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                //Set the text in TextBox1 to the path of the selected folder
+                textBox1.Text = (Path.GetFullPath(fbd.SelectedPath)).ToString();
+            }
+
+            else
+            {
+                //If there is an error of some sort, just close the window and have them try again
+                //I could put a try/catch here that would display the error, but the only reason this ever fails is because of permissions issues
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Set local string variables
+            string dirPath = textBox1.Text;
+            string strOld = textBox2.Text;
+            string strNew = textBox3.Text;
+
+            //Set local integer variables
+            int successfulInt = 0;
+            int failedInt = 0;
+
+            //Create local Lists
+            List<string> successfulList = new List<string>();
+            List<string> failedList = new List<string>();
+
+            //Populate information for the directory selected in Step 1
+            DirectoryInfo di = new DirectoryInfo(dirPath);
+
+            //Tells the DirectoryInfo instance (above) to get a list of the files from the selected folder then, for each file in the directory,
+            foreach (FileInfo fi in di.GetFiles())
+            {
+                //Set its file path as a local variable
+                string filePath = (Path.GetFullPath(fi.FullName)).ToString();
+
+                try
+                { 
+                    //Read the file and load it into a local variable named "fileRead"
+                    string fileRead = File.ReadAllText(filePath);
+                    //Create a new local variable that holds the contents of fileRead after replacing the text
+                    string fileTmp = fileRead.Replace(strOld, strNew);
+
+                    //Overwrite the file with the new text
+                    File.WriteAllText(filePath, fileTmp);
+
+                    //Count how many files were successfully modified
+                    successfulInt++;
+                    //Add those successful files to a list of strings
+                    successfulList.Add(DateTime.Now.ToShortTimeString() + ": Successfully replaced text in file - (" + filePath + ")");
+                }
+
+                catch (Exception ex)
+                {
+                    //Count how many files failed to be modified
+                    failedInt++;
+                    //Add the files that failed to a list of strings
+                    failedList.Add(DateTime.Now.ToShortTimeString() + ": Failed to replace text in file - (" + filePath + ")" + Environment.NewLine + "Error Details: " + ex.Message + Environment.NewLine + Environment.NewLine);
+                    //Even though the current file failed, we want to continue the operation until all files have been touched
+                    continue;
+                }
+            }
+
+            //Set local variable for user's desktop path
+            string userDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Set local variable for the path of the log file we're going to create
+            string logPath = userDesktop + @"\TextReplaceLog-" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString() + ".log";
+            //Combine all errors into a string
+            string errorsToString = string.Join(Environment.NewLine, failedList.ToArray());
+            //Combine all successes into a string
+            string successesToString = string.Join(Environment.NewLine, successfulList.ToArray());
+            //Set local variable for the text of the full log that we're creating
+            string logStr = "======= ERRORS: " + failedInt.ToString() + " =======" + Environment.NewLine + errorsToString + Environment.NewLine + Environment.NewLine + "======= Processed Files: " + successfulInt.ToString() + " =======" + Environment.NewLine + successesToString;
+            //Create the log file in the path from above with the full text ^
+            File.WriteAllText(logPath, logStr);
+
+            //Display a message to the user that the operation completed successfully, inform them of any errors along the way and show them the path where the log file was saved
+            MessageBox.Show("Operation completed successfully with " + failedInt.ToString() + " errors." + Environment.NewLine + Environment.NewLine + "Log saved to: " + logPath, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //This could be changed to ask if they wanted to open the log file, like this:
+            /**
+             * if (MessageBox.Show("Operation completed successfully! Do you want to open the log file?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+             * {
+             *      Process.Start(logPath);
+             * }
+             * 
+             * else
+             * {
+             *      return;
+             * }
+             * */
+
+            return;
+        }
+    }
+}
